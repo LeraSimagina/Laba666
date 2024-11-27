5d9cf3bb64e39feecc81b6c840b7b039cadd864b
LeraSimagina <lerasimagina025@gmail.com>
2024-11-27 20:53:59 +0700

    11 Превращаем Crime в сущность (Crime.kt)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
 }
 
 android {
-    namespace = "com.example.laba666"
+    namespace = "com.bignerdranch.android.criminalintent"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.laba666"
-        minSdk = 24
+        applicationId = "com.bignerdranch.android.criminalintent"
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -42,8 +43,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
