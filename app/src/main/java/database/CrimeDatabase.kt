2455e3159090e63e0ed01cd44daf881064579c92
LeraSimagina <lerasimagina025@gmail.com>
2024-11-27 20:59:21 +0700

    Создание класса базы данных

--- app/src/main/java/database/CrimeDatabase.kt
+++ app/src/main/java/database/CrimeDatabase.kt
@@ -1,4 +1,10 @@
 package database
 
-class CrimeDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.laba666.Crime
+
+@Database(entities = [ Crime::class ],
+    version=1)
+abstract class CrimeDatabase : RoomDatabase() {
 }
