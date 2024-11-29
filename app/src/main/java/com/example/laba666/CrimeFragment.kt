52db02bf466745fd9ebfc5cd3a1829527ff6623c
LeraSimagina <lerasimagina025@gmail.com>
2024-11-29 19:49:36 +0700

    Назначение целевого фрагмента (CrimeFragment.kt)

--- app/src/main/java/com/example/laba666/CrimeFragment.kt
+++ app/src/main/java/com/example/laba666/CrimeFragment.kt
@@ -18,6 +18,7 @@ import java.util.UUID
 private const val TAG = "CrimeFragment"
 private const val ARG_CRIME_ID = "crime_id"
 private const val DIALOG_DATE = "DialogDate"
+private const val REQUEST_DATE = 0
 class CrimeFragment : Fragment() {
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
@@ -94,6 +95,7 @@ class CrimeFragment : Fragment() {
         }
         dateButton.setOnClickListener {
             DatePickerFragment.newInstance(crime.date).apply {
+                setTargetFragment(this@CrimeFragment, REQUEST_DATE)
                 show(this@CrimeFragment.requireFragmentManager(), DIALOG_DATE)
             }
         }
